d6188752413930670da867447e4044e248df34ca
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-08-15 10:42:49 +0200

    created table with relevent info

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,5 +1,71 @@
+import { useState } from "react";
+
 export default function CreateUser(){
+
+    //maintain state of input boxes
+    const [inputs, setInputs] =useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setInputs(values => ({...values, [name]: value}));
+    }
+
+    //prevent auto refresh when submit button is pressed
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        
+        console.log(inputs);
+    }
+
     return (
-        <h1>Create User</h1>
+        <div>
+            <h1>Create User</h1>
+            <form onSubmit={handleSubmit}>
+                <table cellSpacing='10'>
+                    <tbody>
+                        <tr>
+                            <th>
+                                <label>Name: </label>
+                            </th>
+                            <td>
+                                <input type='text' name='name' onChange={handleChange}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <label>Email: </label>
+                            </th>
+                            <td>
+                                <input type='email' name='email' onChange={handleChange}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <label>Mobile: </label>
+                            </th>
+                            <td>
+                                <input type='text' name='mobile' onChange={handleChange}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            
+                            <td>
+                                <button>Save</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+                
+                <br/>
+                
+
+                
+
+            </form>
+        </div>
+        
     )
 }
