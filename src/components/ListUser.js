def6ae2a5b496f0238a8fd4e83e054ee1ff2ff8b
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-08-16 13:47:33 +0200

    Displaying the list on the page

--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -1,5 +1,51 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function ListUser(){
+    
+    const [users, setUsers] = useState([]);
+    
+    //get the data from the database
+    useEffect(() => {
+        getUsers();
+    }, []); // Set default value to blank
+
+    function getUsers(){
+        axios.get('http://localhost/usertestapi/users/').then(function(response){
+            console.log(response.data);
+            setUsers(response.data)
+        });
+    }
+    
     return (
-        <h1>List User</h1>
+        <div>
+            <h1>List Users</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Mobile</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user, key)=>
+                        <tr key={key}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.mobile}</td>
+                            <td>
+                                <Link to={`user/${user.id}/edit`} style={{marginRight: "1rem"}}>Edit</Link>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
     )
 }
